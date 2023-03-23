e1784e5e55b7cfbe9c6414781783bf61bc121cea
Mian Dawood Shah <miandawood@codeforpakistan.org>
2023-03-23 23:44:47 +0500

    multiple sites indexing on bing api

--- Bingmultisites.php
+++ Bingmultisites.php
@@ -2,13 +2,14 @@
 if($_POST['send'] && $_POST['send'] == 'url'){
   
 	$input = $_POST['urls'];
+    $siteUrl = $_POST['site'];
 	$array = explode(",", $input);
 	$values = array_values($array);
 	
 	$apikey = "your-api-key";
 	//$siteUrl = "https://bitcoderlabs.com";
 
-	$siteUrl = "http://gtvcbpeshawar.edu.pk/";
+	//$siteUrl = "http://gtvcbpeshawar.edu.pk/";
 	$urlList = $values;
 	$data = array(
 		"siteUrl" => $siteUrl,
@@ -30,9 +31,6 @@ if($_POST['send'] && $_POST['send'] == 'url'){
 
 }
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -80,17 +78,53 @@ if($_POST['send'] && $_POST['send'] == 'url'){
 		input[type="submit"]:hover {
 			background-color: #3e8e41;
 		}
+
+        select {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #fff;
+  font-size: 16px;
+  appearance: none; /* remove default appearance */
+  -webkit-appearance: none; /* for Safari */
+  background-image: url('down-arrow.svg'); /* add custom arrow icon */
+  background-repeat: no-repeat;
+  background-position: right center;
+  background-size: 16px 16px;
+  cursor: pointer;
+}
+
+select:hover,
+select:focus {
+  border-color: #4CAF50;
+}
+
+select option {
+  background-color: #fff;
+  color: #000;
+}
+
 	</style>
 </head>
 <body>
-<form action="" method="post">
-		<!--<label for="name">Name:</label>-->
-		<label for="message">Urs for Indexing:</label>
-		<textarea id="message" name="urls" placeholder="Enter your Urls"></textarea>
-<input type="hidden" name="send" value="url">
+	<form action="" method="post">
+		<label for="site">Site:</label>
+		<select id="site" name="site">
+			<option value="https://bitcoderlabs.com/">BitcoderLabs</option>
+			<option value="https://gtvcbpeshawar.edu.pk/">gtvcbpeshawar</option>
+			<option value="https://franchisepk.com/">Franchise</option>
+		</select>
+
+		<label for="message">URLs for Indexing:</label>
+		<textarea id="message" name="urls" placeholder="Enter your URLs"></textarea>
+
+		<input type="hidden" name="send" value="url">
 		<input type="submit" value="Submit">
 	</form>
-</body>
+</body
 </html>
 
 
